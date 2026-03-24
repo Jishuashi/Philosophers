d4b9292ad8360eabcb2cb74b4afc4a35b0c0558b
Hugo Chartier <chartierhugo098@gmail.com>
2026-03-24 16:27:42 +0100

    Add elapsed time

--- src/includes/utils.h
+++ src/includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 17:37:23 by hchartie          #+#    #+#             */
-/*   Updated: 2026/03/23 19:17:00 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/03/24 16:25:16 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ void		print_err(char *str);
 long long	get_ms_time(long sec, long micro);
 void		ft_init_mutex(t_data *data);
 void		ft_exit(t_data *data);
+long long 	get_elapsed_time(t_data data);
 
 #endif
--- src/utils/time_utils.c
+++ src/utils/time_utils.c
@@ -6,7 +6,7 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 03:38:47 by hchartie          #+#    #+#             */
-/*   Updated: 2026/03/23 03:44:17 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/03/24 16:24:20 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,13 @@
 long long	get_ms_time(long sec, long micro)
 {
 	return ((sec * 1000) + (micro / 1000));
+}
+
+long long get_elapsed_time(t_data data)
+{
+	struct timeval end;
+	
+	gettimeofday(&end, 0);
+	return(get_ms_time(data.st_time.tv_sec, data.st_time.tv_sec)
+	- get_ms_time(end.tv_sec, end.tv_usec));
 }
