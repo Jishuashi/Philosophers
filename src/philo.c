d64c30dbacf7fe1959fb137bbdd886ed36e65f48
Hugo Chartier <chartierhugo098@gmail.com>
2026-03-31 11:47:19 +0200

    Fix conflict

--- src/philo.c
+++ src/philo.c
@@ -6,11 +6,7 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 15:36:25 by hchartie          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2026/03/31 11:02:02 by hchartie         ###   ########.fr       */
-=======
-/*   Updated: 2026/03/24 19:34:03 by hchartie         ###   ########.fr       */
->>>>>>> 14d7b34c8f5b1743603952c15c441673c4fed0ae
+/*   Updated: 2026/03/31 11:46:51 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +24,6 @@ int	main(int ac, char *av[])
 
 void	*routine(void *arg)
 {
-<<<<<<< HEAD
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
@@ -39,11 +34,5 @@ void	*routine(void *arg)
 	{
 		printf("Hello");
 	}
-=======
-	t_philo	*philo = (t_philo *)arg;
-	pthread_mutex_lock(&philo->data->meal_lock);
-	init_last_meal_time(philo);
-	pthread_mutex_unlock(&philo->data->meal_lock);
->>>>>>> 14d7b34c8f5b1743603952c15c441673c4fed0ae
 	return (NULL);
 }
