d480dc016143d39dca2d304da1a36505e00e8f9f
Hugo Chartier <chartierhugo098@gmail.com>
2026-03-31 11:24:56 +0200

    Fix Norm

--- src/utils/ft_usleep.c
+++ src/utils/ft_usleep.c
@@ -6,11 +6,11 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 10:20:53 by hchartie          #+#    #+#             */
-/*   Updated: 2026/03/31 11:06:52 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/03/31 11:24:45 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"../includes/philo.h"
+#include "../includes/philo.h"
 
 void	ft_usleep(int wait, t_data *data)
 {
@@ -19,10 +19,10 @@ void	ft_usleep(int wait, t_data *data)
 
 	gettimeofday(&current, NULL);
 	gettimeofday(&start, NULL);
-	while ((get_ms_time(current.tv_sec, current.tv_usec) 
-	- get_ms_time(start.tv_sec, start.tv_usec)) < wait)
+	while ((get_ms_time(current.tv_sec, current.tv_usec)
+			- get_ms_time(start.tv_sec, start.tv_usec)) < wait)
 	{
-		if(!check_sim(data))
+		if (!check_sim(data))
 			break ;
 		usleep(500);
 		gettimeofday(&current, NULL);
