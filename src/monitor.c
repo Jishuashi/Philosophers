1cb4be620616cf378571ca3adaae8c380e21a801
Hugo Chartier <chartierhugo098@gmail.com>
2026-04-08 11:45:24 +0200

    Fix norm monitor

--- src/monitor.c
+++ src/monitor.c
@@ -6,18 +6,18 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 13:51:33 by hchartie          #+#    #+#             */
-/*   Updated: 2026/04/08 11:39:21 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/04/08 11:45:09 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
 
-static	int check_mon(t_data *data, int idx);
+static int	check_mon(t_data *data, int idx);
 
 void	monitor(t_data *data)
 {
 	int					i;
-	
+
 	while (check_sim(data))
 	{
 		i = 0;
@@ -34,17 +34,17 @@ void	monitor(t_data *data)
 	}
 }
 
-static	int check_mon(t_data *data, int idx)
+static int	check_mon(t_data *data, int idx)
 {
-	struct	timeval		current;
+	struct timeval		current;
 	long long			lastmeal;
-	
+
 	gettimeofday(&current, 0);
 	pthread_mutex_lock(&data->meal_lock);
 	lastmeal = data->philos[idx].last_meal;
 	pthread_mutex_unlock(&data->meal_lock);
 	if (get_ms_time(current.tv_sec, current.tv_usec)
 		- lastmeal > data->time_to_die)
-		return	(1);
+		return (1);
 	return (0);
 }
